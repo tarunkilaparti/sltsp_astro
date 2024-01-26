d0f128bdbce27792ed8719cea6c76b7700d0bfad
tarunkilaparti <157494429+tarunkilaparti@users.noreply.github.com>
2024-01-26 17:48:56 +0530

    SltspApplication.java
    Create SltspApplication.java

---
+++ src/main/java/com/jothishy/sltsp/SltspApplication.java
@@ -0,0 +1,13 @@
+package com.jothishy.sltsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SltspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SltspApplication.class, args);
+	}
+
+}
